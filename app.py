89bcdd6ec10c2e3bf06822cbfa8c819c27cd29f9
Chardinal Martin Butarbutar <chardinal.butarbutar174@gmail.com>
2026-05-31 14:33:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from visualizations import (
 # Page Config
 
 st.set_page_config(
-    page_title="ITCareerMatch — Recruitment Analytics",
+    page_title="ITCareerMatch - Recruitment Analytics",
     page_icon="chart_with_upwards_trend",
     layout="wide",
     initial_sidebar_state="expanded"
